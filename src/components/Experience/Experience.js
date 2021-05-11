78d1eb25b36c981d41bafd7a15df87cc462d0da8
Shubham Vibhute <61193423+Shubham8656@users.noreply.github.com>
2021-05-11 12:57:18 +0530

    Add files via upload

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import  './Experience.css';
+
+export default function Education(){
+    return(
+        <div className='education' id='exp'>
+            <h2 id='project'>WORK EXPERIENCE</h2>
+            <div className='edu-container'>
+                <div className='card'>
+                    <div id='organisation'>
+                        <p id='school'>TCS, Pune</p>                        
+                        <p id='time'>March 2020 - Present</p>
+                    </div>
+                    <div id='course'>Assistant System Engg-Trainee</div>
+                </div>
+                <div className='card'>
+                    <div id='organisation'>
+                        <p id='school'>Deccanloginc OPC Pvt Ltd, Pune</p>                        
+                        <p id='time'> Dec 2020 - Feb 2021</p>
+                    </div>
+                    <div id='course'>Associate Software Developer (Front-End)</div>
+                </div>
+                {/* <div className='card'>
+                    <div id='organisation'>
+                        <p id='school'>Government Polytechnic, Solapur</p>                        
+                        <p id='time'> 2014 - 2017</p>
+                    </div>
+                    <div id='course'>Diploma ( Mechanical )</div>
+                </div> */}
+            </div>
+        
+        </div>
+    );
+}
