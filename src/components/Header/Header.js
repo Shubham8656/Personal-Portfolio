f6391b95698fcd1bff8fdb7ab26abaf302244c5d
Shubham Vibhute <61193423+Shubham8656@users.noreply.github.com>
2021-05-11 12:55:10 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,7 @@ export default function Header(){
             <a href='#work'><div id='tab'>Work</div></a>
             <a href='#skill'><div id='tab'>Skill</div></a>
             <a href='#educ'><div id='tab'>Education</div></a>
+            <a href='#exp'><div id='tab'>Work Experience</div></a>
             <a href='#contact'><div id='tab'>Contact Us</div></a>
         </div>
     );
