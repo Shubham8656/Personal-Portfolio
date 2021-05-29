e6cdc0b56a3230c1141ade6eecff6f158c591eb2
Shubham Vibhute <61193423+Shubham8656@users.noreply.github.com>
2021-05-29 13:27:15 +0530

    Update Work.js

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,6 +3,7 @@ import  './Work.css';
 import weather from '../image/weather.jpg';
 import ecommerce from '../image/ecommerce.jpg';
 import covid from '../image/covid.png';
+import mucormycosis from '../image/mucormycosis.png';
 import zomato from '../image/zomato.jpg';
 import payment from '../image/payment.jpg';
 import event from '../image/event.jpg';
@@ -12,32 +13,37 @@ export default function Work(){
         <div className='work-container' id='work'>
             <h2 id='project'>CHECK OUT SOME OF MY PROJECTS</h2>
             <div className='project-container'>
-                <a href='https://priceless-turing-84fb4f.netlify.app/'>
+                <a href='http://priceless-turing-84fb4f.netlify.app/'>
                     <img src={ecommerce} alt=''/>
                     <h3>Online Grossary Store</h3>
                 </a>
                 
-                <a href='https://covid-self-assessment.netlify.app/'>
+                <a href='http://covid-self-assessment.netlify.app/'>
                     <img src={covid} alt=''/>
                     <h3>Covid Self Assessment</h3>
                 </a>
 
-                <a href='https://modest-euler-3abbb5.netlify.app/'>
+                <a href='https://mucormycosis-analysis.netlify.app/'>
+                    <img src={mucormycosis} alt=''/>
+                    <h3>Mucormycosis Analysis</h3>
+                </a>
+
+                <a href='http://modest-euler-3abbb5.netlify.app/'>
                     <img src={weather} alt=''/>
                     <h3>Weather Application </h3>
                 </a>
 
-                <a href='https://shubham8656.github.io/zomato-replica/'>
+                <a href='http://shubham8656.github.io/zomato-replica/'>
                     <img src={zomato} alt=''/>
                     <h3>Zomato Replica </h3>
                 </a>
 
-                <a href='https://practical-gates-13307b.netlify.app/'>
+                <a href='http://practical-gates-13307b.netlify.app/'>
                     <img src={payment} alt=''/>
                     <h3>Payment Portal</h3>
                 </a>
 
-                <a href='https://quizzical-hamilton-65384f.netlify.app/'>
+                <a href='http://quizzical-hamilton-65384f.netlify.app/'>
                     <img src={event} alt=''/>
                     <h3>Create Event</h3>
                 </a>
